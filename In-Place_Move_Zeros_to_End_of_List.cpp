2568168e27369d4d07c3141f6ba17966c3810d42
shishukumar <shishubabu94@gmail.com>
2021-04-17 21:09:47 +0530

    new file added

---
+++ In-Place_Move_Zeros_to_End_of_List.cpp
@@ -0,0 +1,12 @@
+vector<int> solve(vector<int>& nums) {
+    int n = nums.size();
+    int count = 0;
+    for(int i=0;i<n;i++){
+        if(nums[i] != 0){
+        swap(nums[i], nums[count]);
+        count++;
+        }
+    }
+    return nums;
+    
+}
