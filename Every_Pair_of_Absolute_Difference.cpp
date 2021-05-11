e23ba5162a106e5cc775fb2995ed5bff8682dfa2
shishukumar <shishubabu94@gmail.com>
2021-05-11 23:42:26 +0530

    new file added

--- Every_Pair_of_Absolute_Difference.cpp
+++ Every_Pair_of_Absolute_Difference.cpp
@@ -1,6 +1,9 @@
-int sum = 0;
-    int n = arr.size();
-    for (int i = n - 1; i >= 0; i--) sum = (sum + i * arr[i] - (n - 1 - i) * arr[i]) % 1000000007;
-
-    sum = (sum << 1) % 1000000007;
-    return sum;
+int solve(vector<int>& nums) {
+    int sum = 0;
+    int n = nums.size();
+    for (int i = n-1; i >= 0; i--){
+        sum = (sum + i * nums[i] - (n - 1 - i) * nums[i]) % 1000000007;
+  }    
+        sum = (sum << 1) % 1000000007;
+        return sum;
+}
