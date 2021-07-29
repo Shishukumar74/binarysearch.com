4fdf26d9c5e88406fb29597926ff37f3dc32e48c
shishukumar <shishubabu94@gmail.com>
2021-07-29 23:34:19 +0530

    new file added

---
+++ Add_Binary_Numbers.cpp
@@ -0,0 +1,11 @@
+string solve(string a, string b) {
+   string res = "";
+    int carry = 0;
+    for (int i = a.size() - 1, j = b.size() - 1; i >= 0 || j >= 0; i--, j--) {
+        int m = (i >= 0 && a[i] == '1');
+        int n = (j >= 0 && b[j] == '1');
+        res = to_string((m + n + carry) & 1)+res;
+        carry = (m + n + carry) >> 1;
+    }
+    return carry ? '1' + res : res; 
+}
