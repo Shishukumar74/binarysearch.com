bebba21929d2ca247d4efd2891b53eccc4383bbf
shishukumar <shishubabu94@gmail.com>
2021-09-03 23:26:55 +0530

    new file added

---
+++ Special_Product_List.cpp
@@ -0,0 +1,14 @@
+vector<int> solve(vector<int>& nums) {
+   int n = nums.size();
+    vector<int> ans(n, 1);
+
+    int left = 1, right = 1;
+    for (int i = 0; i < n; i++) {
+        ans[i] *= left;
+        ans[n - i - 1] *= right;
+        left *= nums[i];
+        right *= nums[n - i - 1];
+    }
+
+    return ans;
+}
