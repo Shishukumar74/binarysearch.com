4152be9db8d47050257db87268714cf686fde9fc
shishukumar <shishubabu94@gmail.com>
2021-09-12 23:46:17 +0530

    new file added

---
+++ Split_Product.cpp
@@ -0,0 +1,18 @@
+int solve(int n) {
+    if (n <= 1) {
+        return 0;
+    }
+    if (n == 2) {
+        return 1;
+    }
+    else if (n == 3) {
+        return 2;
+    }
+    else if (n % 3 == 0) {
+        return (int)pow(3, n / 3);
+    } else if (n % 3 == 1) {
+        return 2 * 2 * (int)pow(3, (n - 4) / 3);
+    } else {
+        return 2 * (int)pow(3, n / 3);
+    }
+}
