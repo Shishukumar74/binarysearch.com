f77ef098b28a184ebbdd6ac9d56ef86d44a1cc3d
shishukumar <shishubabu94@gmail.com>
2022-01-31 23:44:03 +0530

    new file added

---
+++ Tribonacci_Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n<2) return n;
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        for(int i=2;i<n;i++){
+            int temp = a+b+c;
+            a = b;
+            b = c;
+            c = temp;
+        }
+        return c;
+    }
+};
